3b0893fea568897fd0019f765bab48067097cc66
T. Nishimura <tnish@fastmail.jp>
2021-12-29 04:46:01 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # devpg
+
 Launch a temporary, development instance of Postgresql on demand
+
+## Introduction
+
+`devpg` is a small script which:
+* automates the process of launching a PostgreSQL Docker container
+* initializes the database with an optional SQL file
+* opens a `psql` session and handles 
+* handles cleanup of the container 
+
+## Prerequisites
+
+This Bash requires you have Docker installed and preconfigured (you should be in the `docker` group, for example). 
+It also requires you have the PostgreSQL client tool (`psql`) installed. 
+
+## Installation
+
+To install, simply place the `devpg` script somewhere in your `$PATH`.
+
+## Usage
+
+    Usage:
+        devpg [OPTION]...
+
+    Options:
+        -h, --help
+        -i, --init-script=FILENAME   initialize the database with some sql
+        -p, --port=INT               local port (default: 15432)
+        -P, --password=STRING        database password (default: password)
+        -t, --tmp-directory=DIRNAME  temporary directory (default: /tmp/devpg-$PID)
+
+At its most basic, running `devpg` without any arguments will create an instance of PostgreSQL and opens a `psql` session to the instance for you.
+You can also connect to the instance from any application on localhost with the port and password specified. 
+
+Additionally, you can provide an SQL initialization script using `-i`. 
+You can place DDL statements (`create table`, etc) and sample data (using `insert`) in this file to quickly test your application against your schema. 
+A small sample database `sample.sql` is included to show its functionality.
---
+++ sample.sql
@@ -0,0 +1,78 @@
+
+-- Sample database of courses at a hypothetical university 
+
+create table departments (
+    department_id text primary key,
+    department_name text not null
+);
+
+create table professors (
+    professor_id int primary key,
+    first_name text not null,
+    middle_name text,
+    last_name text not null,
+    department_id text not null references departments(department_id)
+);
+
+create table classrooms (
+    classroom_id int primary key,
+    building_name text not null,
+    room_number int not null
+);
+
+create table courses (
+    course_id text primary key,
+    course_name text not null,
+    professor_id int not null references professors(professor_id),
+    department_id text not null references departments(department_id),
+    classroom_id int not null references classrooms(classroom_id),
+    start_date date,
+    end_date date,
+    finals_time timestamptz
+);
+
+insert into departments(department_id, department_name) 
+values ('MATH', 'Mathematics'),
+       ('MCB', 'Molecular and Cell Biology'),
+       ('PHYS', 'Physics');
+
+insert into professors (professor_id, first_name, middle_name, last_name, department_id)
+values
+    (1 , 'Melvin'    , null       , 'Calvin'    , 'MCB'),
+    (2 , 'Wendell'   , 'Meredith' , 'Stanley'   , 'MCB'),
+    (3 , 'Ernest'    , 'Orlando'  , 'Lawrence'  , 'PHYS'),
+    (4 , 'Donald'    , 'Arthur'   , 'Glaser'    , 'PHYS'),
+    (5 , 'Shoshichi' , null       , 'Kobayashi' , 'MATH'),
+    (6 , 'David'     , 'Harold'   , 'Blackwell' , 'MATH'),
+    (7 , 'Michel'    , null       , 'Loève'     , 'MATH');
+
+insert into classrooms (classroom_id, building_name, room_number)
+values 
+    (1  , 'Evans'   , 1)   ,
+    (2  , 'Evans'   , 33)  ,
+    (3  , 'Evans'   , 16)  ,
+    (4  , 'Evans'   , 60)  ,
+    (5  , 'VSLB'    , 100) ,
+    (6  , 'VSLB'    , 101) ,
+    (7  , 'VSLB'    , 102) ,
+    (8  , 'VSLB'    , 207) ,
+    (9  , 'Leconte' , 7)   ,
+    (10 , 'Leconte' , 6)   ,
+    (11 , 'Leconte' , 1);
+    
+
+insert into courses (department_id, course_id, course_name, professor_id, classroom_id, start_date, end_date, finals_time)
+values
+    ('MATH' , 'MATH 110'  , 'Linear Algebra'       , 5 , 1, '2021-08-18', '2021-12-09',  '2021-12-13 17:30:00 America/Los_Angeles'),
+    ('MATH' , 'MATH 113'  , 'Abstract Algebra'     , 7 , 1, '2021-08-19', '2021-12-10',  '2021-12-15 08:00:00 America/Los_Angeles'),
+    ('MATH' , 'MATH 104'  , 'Real Analysis'        , 6 , 2, '2021-08-18', '2021-12-09',  '2021-12-14 15:00:00 America/Los_Angeles'),
+    ('MATH' , 'MATH 185'  , 'Complex Analysis'     , 6 , 3, '2021-08-19', '2021-12-10',  '2021-12-17 15:00:00 America/Los_Angeles'),
+    ('MCB'  , 'MCB 100'   , 'Biochemistry'         , 1 , 5, '2021-08-18', '2021-12-09',  '2021-12-17 13:30:00 America/Los_Angeles'),
+    ('MCB'  , 'MCB 140'   , 'Genetics'             , 1 , 5, '2021-08-19', '2021-12-10',  '2021-12-13 11:00:00 America/Los_Angeles'),
+    ('MCB'  , 'MCB 150'   , 'Immunology'           , 2 , 7, '2021-08-20', '2021-12-01',  '2021-12-15 10:00:00 America/Los_Angeles'),
+    ('MCB'  , 'MCB 160'   , 'Neurobiology'         , 2 , 9, '2021-08-18', '2021-12-09',  '2021-12-20 20:00:00 America/Los_Angeles'),
+    ('PHYS' , 'PHYS 141'  , 'Solid State Physics'  , 3 , 9, '2021-08-20', '2021-12-10',  '2021-12-15 09:30:00 America/Los_Angeles'),
+    ('PHYS' , 'PHYS 137A' , 'Quantum Mechanics'    , 3 , 10, '2021-08-18', '2021-12-09', '2021-12-16 14:00:00 America/Los_Angeles'),
+    ('PHYS' , 'PHYS 177'  , 'Molecular Biophysics' , 4 , 11, '2021-08-18', '2021-12-09', '2021-12-16 17:00:00 America/Los_Angeles');
+
+
